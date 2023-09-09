b3cc68d9bd53d44078f2a18890b4521a287aecfe
aasem shoshari <117245999+aasemShoshari1998@users.noreply.github.com>
2023-09-09 16:32:49 +0300

    Update ReportsComponent.jsx

--- client/src/components/componentContainer/reports/ReportsComponent.jsx
+++ client/src/components/componentContainer/reports/ReportsComponent.jsx
@@ -53,7 +53,7 @@ function ReportsComponent() {
         month: month.current.value,
         year: year.current.value,
       };
-      const response = await fetch("https://emp-dashboard-client.vercel.app/report", {
+      const response = await fetch("https://emp-dashboard-api.vercel.app/report", {
         method: "POST",
         headers: {
           "Content-type": "application/json",
