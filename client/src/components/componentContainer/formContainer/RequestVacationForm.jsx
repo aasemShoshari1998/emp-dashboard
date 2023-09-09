08a55849a1548430d13e04b9438ac0f0091c49c5
aasem shoshari <117245999+aasemShoshari1998@users.noreply.github.com>
2023-09-09 19:44:39 +0300

    Update RequestVacationForm.jsx

--- client/src/components/componentContainer/formContainer/RequestVacationForm.jsx
+++ client/src/components/componentContainer/formContainer/RequestVacationForm.jsx
@@ -49,8 +49,6 @@ function RequestVacationForm() {
       };
       await fetch(`https://emp-dashboard-api.vercel.app/request-vacation`, {
         method: "POST",
-                mode: "no-cors",
-
         headers: {
           "Content-type": "application/json",
         },
