4f1c212be2c4168b7137acc389c62ce76fd6d9a3
aasem shoshari <117245999+aasemShoshari1998@users.noreply.github.com>
2023-09-09 16:22:19 +0300

    Update Edit.jsx

--- client/src/components/componentContainer/tableContainer/windows/Edit.jsx
+++ client/src/components/componentContainer/tableContainer/windows/Edit.jsx
@@ -14,7 +14,7 @@ function Edit({ content, table, setEdit, state, updateState }) {
   const handleEdit = async (e) => {
     dispatch({ type: "DARKEN", payload: false });
     table === "Employee"
-      ? await fetch("emp-dashboard-api.vercel.app/edit-employee", {
+      ? await fetch("https://emp-dashboard-client.vercel.app/edit-employee", {
           method: "POST",
           headers: {
             "Content-type": "application/json",
@@ -24,7 +24,7 @@ function Edit({ content, table, setEdit, state, updateState }) {
           setEdit(false);
           getEmployees();
         })
-      : await fetch("emp-dashboard-api.vercel.app/edit-salary", {
+      : await fetch("https://emp-dashboard-client.vercel.app/edit-salary", {
           method: "POST",
           headers: {
             "Content-type": "application/json",
